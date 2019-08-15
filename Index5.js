3323bbf92a905ed50b9a7cf2c1d139faa5724cff
Soro Cat <sorousha326@gmail.com>
2019-08-15 14:44:15 -0700

    add changes in html

---
+++ Index5.js
@@ -0,0 +1,5 @@
+var testContact = new contact("Ada", "Lovelace", "503-555-1111");
+
+contact {firstName: "Ada", lastName: "Lovelace", phoneNumber: "503-555-1111"}
+
+testContact.fullName();
--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+// business logic
+function contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
 $(document).ready(function() {
+  $("form#contact").submit(function(event) {
