4b6deda4a0491bf9b9ffa2ae4aaa3601eff08bec
Soro Cat <sorousha326@gmail.com>
2019-08-22 14:36:39 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ var sun = { name: "San Ramon"};
 var sun2 = { name: "San Fransisco"};
 var sun3 = { name: "Los Angeles"};
 var arab = { name: "Doha"};
+var beach = { name: "Orlando"};
+
+var usa = { name: "United States of America", cities: [espanol, sun, sun2, sun3, beach] };
+var spain = { name: "Spain", cities: [espanol] };
+var qatar = { name: "Qatar", cities: [arab] };
 
 
 
