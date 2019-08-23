80664131bad95a2a864e6b96c44ff3a23928dd12
Soro Cat <sorousha326@gmail.com>
2019-08-23 10:36:36 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-// business logic
-var espanol = { name: "Marbella"};
+// business logic for Destination
+var espanol = { name: "Marbella"}; {
 var sun = { name: "San Ramon"};
 var sun2 = { name: "San Fransisco"};
 var sun3 = { name: "Los Angeles"};
 var arab = { name: "Doha"};
 var beach = { name: "Orlando"};
+}
 
-var usa = { name: "United States of America", cities: [espanol, sun, sun2, sun3, beach] };
+var usa = { name: "United States of America", cities: [espanol, sun, sun2, sun3, beach] }; {
 var spain = { name: "Spain", cities: [espanol] };
 var qatar = { name: "Qatar", cities: [arab] };
+}
+
 
 usa.cities.forEach(function(city) {
   console.log("Let's go to " + city.name + "!");
@@ -21,6 +24,15 @@ spain.cities.forEach(function(city) {
 qatar.cities.forEach(function(city) {
   console.log("Let's go to " + city.name + "!");
 });
+
+var la = { name: "Los Angeles" };
+usa.cities.push(la);
+
+var sr = { name: "San Ramon" };
+usa.cities.push(sr);
+
+var sf = { name: "San Fransisco" };
+usa.cities.push(sf);
 
 
 
